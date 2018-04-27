c3824f791edadd4a0e4b0d2ef9a4c8feb000528d
Deepak Grover <deepakgrover333@gmail.com>
2018-04-27 19:13:30 +0530

    update Navbar component - add mobile menu

--- src/atoms/Navbar/Navbar.js
+++ src/atoms/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'react-emotion';
+import styled, { css } from 'react-emotion';
 
 import base from '../../shared/base';
 import font from '../../shared/font';
@@ -16,6 +16,7 @@ const Nav = styled('nav')`
 	left: 0;
 	padding: 0 15px;
 	display: flex;
+	z-index: 10;
 	justify-content: space-between;
 	align-items: center;
 	background-color: transparent;
@@ -41,24 +42,127 @@ const Nav = styled('nav')`
 			margin: ${props => (props.gutter ? `0 0 0 ${props.gutter}` : '0 0 0 40px')};
 		}
 	}
+
+	@media all and (max-width: 767px) {
+		.list {
+			position: fixed;
+			top: 0;
+			width: 100%;
+			height: 100%;
+			max-width: 400px;
+			right: -400px;
+			margin: 0;
+			padding: 0;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			background-color: #f3f3f3;
+			transition: all .3s ease;
+
+			li {
+				margin: 0;
+				width: 100%;
+				padding: 12px 0;
+			}
+		}
+
+		&.is-open .list {
+			right: 0;
+		}
+	}
 `;
 
+const toggleMenu = css`
+	display: none;
+	border: 0;
+	background-color: transparent;
+	box-shadow: none;
+	outline: none;
+	user-select: none;
+	z-index: 1;
+	cursor: pointer;
+
+	@media all and (max-width: 767px) {
+		display: block;
+	}
+
+	span {
+		display: block;
+		width: 33px;
+		height: 4px;
+		margin-bottom: 5px;
+		position: relative;
+		background: #cdcdcd;
+		border-radius: 3px;
+		z-index: 1;
+		transform-origin: 4px 0px;
+		transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+					background 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+					opacity 0.55s ease;
+
+		&:first-child {
+			transform-origin: 0% 0%;
+		}
+
+		&:nth-last-child(3) {
+			transform-origin: 0% 100%;
+		}
+	}
+
+	&.active span {
+		opacity: 1;
+		transform: rotate(-45deg) translate(-2px, -1px);
+		background: #232323;
+
+		&:nth-last-child(2) {
+			opacity: 0;
+			transform: rotate(0deg) scale(0.2, 0.2);
+		}
+
+		&:nth-last-child(3) {
+			transform: rotate(45deg) translate(-2px, -1px);
+		}
+	}
+`;
 
 class Navbar extends Component {
-	static Logo = ({ children }) => (<div>{children}</div>)
-	static List = ({ children }) => (<ul className="list">{children}</ul>)
+	static Logo = ({ children }) => (<div>{children}</div>);
+	static List = ({ children }) => (<ul className="list">{children}</ul>);
+
+	state = {
+		showMenu: false,
+	};
+
+
+	toggleMenu = () => {
+		this.setState(({ showMenu }) => ({
+			showMenu: !showMenu,
+		}));
+	}
 
 	render() {
-		const { children, ...props } = this.props;
+		const { children, className, ...props } = this.props;
+		const isOpen = this.state.showMenu ? 'is-open' : '';
 		return (
-			<Nav {...props}>
+			<Nav {...props} className={`${className || ''} ${isOpen}`}>
 				{children}
+
+				<button
+					className={`${toggleMenu} ${this.state.showMenu ? 'active' : ''}`}
+					onClick={this.toggleMenu}
+				>
+					<span />
+					<span />
+					<span />
+				</button>
 			</Nav>
 		);
 	}
 }
 
 Navbar.propTypes = {
+	className: PropTypes.string,
 	children: PropTypes.any, // eslint-disable-line
 };
 
