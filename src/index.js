ec657100033bad74f84d4bc9514644440e2ccfa3
Deepak Grover <deepakgrover333@gmail.com>
2018-04-28 23:12:01 +0530

    export updated components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 export Button from './atoms/Button';
+export Navbar from './atoms/Navbar';
+export GithubButton from './atoms/GithubButton';
+export Logo from './atoms/Logo';
 export * from './atoms/typography';
 export Flex from './layouts/Flex';
