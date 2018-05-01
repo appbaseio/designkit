d25dd9a485eaba02f29387a1563fc06f33d32663
Deepak Grover <deepakgrover333@gmail.com>
2018-05-01 23:27:10 +0530

    add media queries utils

---
+++ src/shared/mediaQueries.js
@@ -0,0 +1,28 @@
+const breakpoints = {
+	sm: 576,
+	md: 768,
+	lg: 992,
+	xl: 1200,
+};
+
+function queries(size) {
+	return `@media all and (max-width: ${breakpoints[size]}px)`;
+}
+
+function toTitleCase(string) {
+	return string.replace(/\w\S*/g, txt => txt.charAt(0).toUpperCase() + txt.substr(1));
+}
+
+const sizeOrder = ['sm', 'md', 'lg', 'xl'];
+const getProperty = (props, property, size) => {
+	if (props[`${size}${toTitleCase(property)}`]) {
+		return props[`${size}${toTitleCase(property)}`];
+	} else if (size === 'xl') {
+		return props[property];
+	}
+	const index = sizeOrder.indexOf(size);
+	return getProperty(props, property, sizeOrder[index + 1]);
+};
+
+export default queries;
+export { getProperty };
