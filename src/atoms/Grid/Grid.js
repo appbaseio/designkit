c2abb1cd79229bfcb95cecd0f0a212834ba9d775
Deepak Grover <deepakgrover333@gmail.com>
2018-05-02 00:40:03 +0530

    Add Grid component

---
+++ src/atoms/Grid/Grid.js
@@ -0,0 +1,34 @@
+import styled from 'react-emotion';
+import queries, { getProperty } from '../../shared/mediaQueries';
+
+const double = prop => `calc(${prop} * 2)`;
+
+const Grid = styled('div')`
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: space-between;
+
+	& > * {
+		width: ${props => `calc(${100 / props.size}% - ${props.gutter})`};
+		margin-bottom: ${props => double(props.gutter)};
+
+		${queries('lg')} {
+			width: ${props => `calc(${100 / getProperty(props, 'size', 'lg')}% - ${getProperty(props, 'gutter', 'lg')})`};
+			margin-bottom: ${props => double(getProperty(props, 'gutter', 'lg'))};
+		}
+
+		${queries('md')} {
+			width: ${props => `calc(${100 / getProperty(props, 'size', 'md')}% - ${getProperty(props, 'gutter', 'md')})`};
+			margin-bottom: ${props => double(getProperty(props, 'gutter', 'md'))};
+		}
+
+		${queries('sm')} {
+			width: ${props => `calc(${100 / getProperty(props, 'size', 'sm')}% - ${getProperty(props, 'gutter', 'sm')})`};
+			margin-bottom: ${props => double(getProperty(props, 'gutter', 'sm'))};
+		}
+	}
+`;
+
+export default Grid;
