836aa4bde290e9a17dde8f60f21076f248ef8053
Divyanshu Maithani <div.blackcat@gmail.com>
2018-05-29 15:47:05 +0530

    :art: Remove designkit typography in Feature molecule

--- src/molecules/Feature/Feature.js
+++ src/molecules/Feature/Feature.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { node, oneOfType, string, object } from 'prop-types';
 import { css, cx } from 'react-emotion';
 
-import { H3, Title as Desc } from '../../atoms/typography';
+import { H3 } from '../../atoms/typography';
 
 const Image = props => <img alt="Feature" {...props} />;
 const Title = ({ children }) => <H3>{children}</H3>;
-const Description = ({ children }) => <Desc>{children}</Desc>;
+const Description = ({ children }) => (
+	<p css={{ fontSize: '1.125rem' }}>{children}</p>
+);
 
 const feat = css({
 	textAlign: 'center',
