9c7f5bc5ac674ce7dc2f1803769518b00f88e12b
Divyanshu Maithani <div.blackcat@gmail.com>
2018-08-01 12:41:38 +0530

    :wrench: Extend GridLayout props

--- src/layouts/GridLayout/index.js
+++ src/layouts/GridLayout/index.js
@@ -3,6 +3,9 @@ import styled from 'react-emotion';
 const GridLayout = styled('div')(props => ({
 	display: 'grid',
 	gridTemplateColumns: props.gridTemplateColumns || '1fr',
+	gridTemplateRows: props.gridTemplateRows || 'auto',
+	gridGap: props.gridGap || 0,
+	margin: props.margin || 0,
 }));
 
 export default GridLayout;
