e191d5e05a0523f8eca147f050fa0da778afe16c
jyash97 <jyash97@gmail.com>
2019-05-14 17:45:14 +0530

    fix: styling fixes

--- src/molecules/InfoCards/InfoCards-story.js
+++ src/molecules/InfoCards/InfoCards-story.js
@@ -1,30 +1,38 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Play } from 'react-feather';
 import InfoCards from './InfoCards';
-import LazyImage from '../../atoms/LazyImage';
 import { H3 } from '../../atoms/typography';
 
 storiesOf('InfoCards', module).add('default', () => (
-	<InfoCards
-		color={{
-			primary: '#2971fe',
-			title: '#262626',
-			subtitle: '#595959',
-			description: '#bfbfbf',
-			activeDescription: 'white',
-		}}
-	>
+	<InfoCards>
 		<InfoCards.Section>
 			<InfoCards.Card
-				title="Card title"
 				description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
-				subtitle="Lorem Ipsum is simply dummy text"
+				title="Realtime Streaming"
+				subtitle={<a href="#">Read More</a>}
+				iconAtRight
+				icon={
+					<img
+						style={{ maxHeight: 40 }}
+						src="https://appbase.io/static/images/features/Group%2057@2x.png"
+					/>
+				}
+			/>
+			<InfoCards.Card
+				icon={
+					<img src="https://appbase.io/static/images/features/Logs@3x.svg" />
+				}
+				title="Logs"
+				subtitle={<a href="#">Read More</a>}
+				description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
 				iconAtRight
 			/>
 			<InfoCards.Card
-				icon={<Play />}
+				icon={
+					<img src="https://appbase.io/static/images/features/Secure%20Copy@3x.svg" />
+				}
 				title="Card Icon"
+				subtitle="Read More"
 				description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
 				iconAtRight
 			/>
@@ -33,16 +41,33 @@ storiesOf('InfoCards', module).add('default', () => (
 		<InfoCards.MainSection>
 			{selectedItem =>
 				selectedItem ? (
-					<div style={{ textAlign: 'center', padding: '10px' }}>
-						<H3>{selectedItem.title}</H3>
+					<div
+						style={{
+							textAlign: 'center',
+							padding: '10px',
+							height: '100%',
+							boxSizing: 'border-box',
+							background: '#225dd4',
+							borderRadius: 5,
+							color: '#eaeaea',
+						}}
+					>
+						<H3 style={{ color: 'white' }}>{selectedItem.title}</H3>
 						<p>{selectedItem.description}</p>
 					</div>
 				) : (
-					<LazyImage
-						src="https://images.unsplash.com/photo-1551464885-21348ccc8b67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=951&q=100"
-						alt="Nature Photo"
-						style={{ width: '100%' }}
-					/>
+					<div
+						style={{
+							textAlign: 'center',
+							padding: '10px',
+							height: 250,
+						}}
+					>
+						<img
+							style={{ maxWidth: '100%', maxHeight: '100%' }}
+							src="https://appbase.io/static/images/landing-screen@2x.png"
+						/>
+					</div>
 				)
 			}
 		</InfoCards.MainSection>
@@ -50,11 +75,35 @@ storiesOf('InfoCards', module).add('default', () => (
 		<InfoCards.Section>
 			<InfoCards.Card
 				title="Card title"
+				icon={
+					<img
+						style={{ maxHeight: 40 }}
+						src="https://appbase.io/static/images/features/Group%2059@2x.png"
+					/>
+				}
+				subtitle="Read More"
 				description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
 			/>
 			<InfoCards.Card
-				icon={<Play />}
+				icon={
+					<img
+						style={{ maxHeight: 40 }}
+						src="https://appbase.io/static/images/features/Group%2060@2x.png"
+					/>
+				}
 				title="Card Icon"
+				subtitle="Read More"
+				description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
+			/>
+			<InfoCards.Card
+				icon={
+					<img
+						style={{ maxHeight: 40 }}
+						src="https://appbase.io/static/images/features/Group%2063@2x.png"
+					/>
+				}
+				title="Card Icon"
+				subtitle="Read More"
 				description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
 			/>
 		</InfoCards.Section>
--- src/molecules/InfoCards/InfoCards.js
+++ src/molecules/InfoCards/InfoCards.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { css } from 'emotion';
-import { node } from 'prop-types';
+import { node, string } from 'prop-types';
 
-const container = css`
+const container = primaryColor => css`
 	display: grid;
 	grid-template-columns: 1fr 1fr 1fr;
 	grid-gap: 10px;
+	align-items: center;
 	font-family: 'Open Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI',
 		'Roboto', 'Noto Sans', 'Ubuntu', 'Droid Sans', 'Helvetica Neue',
 		sans-serif;
@@ -17,41 +18,79 @@ const container = css`
 		padding: 5px;
 		display: grid;
 		grid-gap: 10px;
+		align-items: center;
 		.card {
-			width: 100%;
 			height: 110px;
-			padding: 5px;
+			padding: 20px 30px;
+			cursor: pointer;
+			box-sizing: border-box;
 			display: grid;
+			grid-template-columns: auto auto;
 			height: 110px;
-			grid-template-columns: auto 1fr;
-			grid-gap: 5px;
+			overflow: hidden;
 			align-items: center;
-			border-left: 3px solid transparent;
-			border-radius: 2px;
-			box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.05);
+			border-right: 3px solid transparent;
+			border-radius: 4px;
+			box-shadow: 0 0px 2px 0 rgba(220, 220, 220, 1);
 			transition: all ease 0.2s;
-			text-align: center;
-
+			.title {
+				margin: 0;
+				color: #262626;
+				font-weight: 400;
+			}
 			.subtitle {
 				height: 0;
 				overflow: hidden;
-			}
-			&:hover {
-				border-left-color: blue;
-				.subtitle {
+				margin: 0;
+				white-space: nowrap;
+				max-width: 200px;
+				text-overflow: ellipsis;
+				color: ${primaryColor};
+				* {
+					color: ${primaryColor};
+				}
+				@media (max-width: 576px) {
 					height: 100%;
 				}
 			}
+			&:hover {
+				border-right-color: ${primaryColor};
+				box-shadow: 0 1px 4px 0 rgba(220, 220, 220, 1);
+				.subtitle {
+					height: 100%;
+					margin-top: 5px;
+				}
+			}
 		}
-	}
-`;
-
-const middleSection = css`
-	height: 235px;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	@media (max-width: 576px) {
-		display: none;
+		.card.with-icon .icon {
+			text-align: left;
+			@media (max-width: 576px) {
+				text-align: right;
+			}
+		}
+		.card.with-icon .content {
+			text-align: right;
+			@media (max-width: 576px) {
+				grid-row: 1;
+				text-align: left;
+			}
+		}
+		.card.right-icon.with-icon .icon {
+			text-align: right;
+		}
+		.card.right-icon.with-icon .content {
+			text-align: left;
+		}
+		.card.right-icon {
+			border-left: 3px solid transparent;
+			border-right: 0;
+		}
+		.card.right-icon:hover {
+			border-left-color: ${primaryColor};
+		}
+		.card.row-reverse {
+			flex-direction: row-reverse;
+		}
 	}
 `;
 
@@ -78,19 +117,26 @@ class InfoCards extends React.Component {
 		<Context.Consumer>
 			{({ onMouseEnter, onMouseLeave }) => (
 				<div
-					className="card"
+					className={`card ${props.iconAtRight ? 'right-icon' : ''} ${
+						props.icon ? 'with-icon' : ''
+					}`}
 					onMouseEnter={() => onMouseEnter(props)}
 					onMouseLeave={onMouseLeave}
 				>
 					{props.icon &&
-						!props.iconAtRight && <div>{props.icon}</div>}
-					<div>
-						<h2 className="title">{props.title}</h2>
+						!props.iconAtRight && (
+							<div className="icon">{props.icon}</div>
+						)}
+					<div className="content">
+						<h3 className="title">{props.title}</h3>
 						{props.subtitle && (
 							<p className="subtitle">{props.subtitle}</p>
 						)}
 					</div>
-					{props.icon && props.iconAtRight && <div>{props.icon}</div>}
+					{props.icon &&
+						props.iconAtRight && (
+							<div className="icon">{props.icon}</div>
+						)}
 				</div>
 			)}
 		</Context.Consumer>
@@ -105,18 +151,16 @@ class InfoCards extends React.Component {
 	);
 
 	static MainSection = props => (
-		<div className={middleSection}>
-			<Context.Consumer>
-				{({ selectedItem }) => props.children(selectedItem)}
-			</Context.Consumer>
-		</div>
+		<Context.Consumer>
+			{({ selectedItem }) => props.children(selectedItem)}
+		</Context.Consumer>
 	);
 
 	render() {
 		const { children, ...props } = this.props;
 		const { selectedItem } = this.state;
 		return (
-			<div className={container}>
+			<div className={container(props.primaryColor)}>
 				<Context.Provider
 					value={{
 						selectedItem,
@@ -133,8 +177,13 @@ class InfoCards extends React.Component {
 	}
 }
 
+InfoCards.defaultProps = {
+	primaryColor: '#225dd4',
+};
+
 InfoCards.propTypes = {
 	children: node,
+	primaryColor: string,
 };
 
 export default InfoCards;
