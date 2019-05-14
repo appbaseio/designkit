be3c625abfd315e5746dd2bab68e622930da5369
Paarmita <paarmita1998@gmail.com>
2019-05-14 15:12:13 +0530

    fix css infocards

--- src/molecules/InfoCards/InfoCards-story.js
+++ src/molecules/InfoCards/InfoCards-story.js
@@ -3,13 +3,12 @@ import { storiesOf } from '@storybook/react';
 import { Play } from 'react-feather';
 import InfoCards from './InfoCards';
 import LazyImage from '../../atoms/LazyImage';
-import { Title } from '../../atoms/typography';
-import Card from '../../atoms/Card';
+import { H3 } from '../../atoms/typography';
 
 storiesOf('InfoCards', module).add('default', () => (
 	<InfoCards
 		color={{
-			primary: '#180Aff',
+			primary: '#2971fe',
 			title: '#262626',
 			subtitle: '#595959',
 			description: '#bfbfbf',
@@ -19,14 +18,14 @@ storiesOf('InfoCards', module).add('default', () => (
 		<InfoCards.Section>
 			<InfoCards.Card
 				title="Card title"
-				description="I am the description"
-				subtitle="Subtitle"
+				description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+				subtitle="Lorem Ipsum is simply dummy text"
 				iconAtRight
 			/>
 			<InfoCards.Card
 				icon={<Play />}
 				title="Card Icon"
-				description="I am the description"
+				description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
 				iconAtRight
 			/>
 		</InfoCards.Section>
@@ -34,10 +33,10 @@ storiesOf('InfoCards', module).add('default', () => (
 		<InfoCards.MainSection>
 			{selectedItem =>
 				selectedItem ? (
-					<Card>
-						<Title>{selectedItem.title}</Title>
+					<div style={{ textAlign: 'center', padding: '10px' }}>
+						<H3>{selectedItem.title}</H3>
 						<p>{selectedItem.description}</p>
-					</Card>
+					</div>
 				) : (
 					<LazyImage
 						src="https://images.unsplash.com/photo-1551464885-21348ccc8b67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=951&q=100"
@@ -51,12 +50,12 @@ storiesOf('InfoCards', module).add('default', () => (
 		<InfoCards.Section>
 			<InfoCards.Card
 				title="Card title"
-				description="I am the description"
+				description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
 			/>
 			<InfoCards.Card
 				icon={<Play />}
 				title="Card Icon"
-				description="I am the description"
+				description="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
 			/>
 		</InfoCards.Section>
 	</InfoCards>
--- src/molecules/InfoCards/InfoCards.js
+++ src/molecules/InfoCards/InfoCards.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { css } from 'emotion';
+import { node } from 'prop-types';
 
 const container = css`
 	display: grid;
-	grid-template-columns: 1fr 0.5fr 1fr;
+	grid-template-columns: 1fr 1fr 1fr;
 	grid-gap: 10px;
+	font-family: 'Open Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+		'Roboto', 'Noto Sans', 'Ubuntu', 'Droid Sans', 'Helvetica Neue',
+		sans-serif;
+	@media (max-width: 576px) {
+		grid-template-columns: 1fr;
+	}
 
 	.card-container {
 		padding: 5px;
@@ -12,13 +19,17 @@ const container = css`
 		grid-gap: 10px;
 		.card {
 			width: 100%;
+			height: 110px;
+			padding: 5px;
 			display: grid;
-			grid-template-columns: auto 1fr;
+			grid-template-columns: auto auto;
 			grid-gap: 5px;
 			align-items: center;
-			border-left: 2px solid transparent;
-			box-shadow: 0 0 2px #e8e8e8;
+			border-left: 3px solid transparent;
+			border-radius: 2px;
+			box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.05);
 			transition: all ease 0.2s;
+			text-align: center;
 
 			.subtitle {
 				height: 0;
@@ -31,6 +42,15 @@ const container = css`
 				}
 			}
 		}
+	}
+`;
+
+const middleSection = css`
+	height: 235px;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	@media (max-width: 576px) {
+		display: none;
 	}
 `;
 
@@ -64,7 +84,7 @@ class InfoCards extends React.Component {
 					{props.icon &&
 						!props.iconAtRight && <div>{props.icon}</div>}
 					<div>
-						<h1 className="title">{props.title}</h1>
+						<h2 className="title">{props.title}</h2>
 						{props.subtitle && (
 							<p className="subtitle">{props.subtitle}</p>
 						)}
@@ -84,9 +104,11 @@ class InfoCards extends React.Component {
 	);
 
 	static MainSection = props => (
-		<Context.Consumer>
-			{({ selectedItem }) => props.children(selectedItem)}
-		</Context.Consumer>
+		<div className={middleSection}>
+			<Context.Consumer>
+				{({ selectedItem }) => props.children(selectedItem)}
+			</Context.Consumer>
+		</div>
 	);
 
 	render() {
@@ -109,5 +131,9 @@ class InfoCards extends React.Component {
 		);
 	}
 }
+
+InfoCards.propTypes = {
+	children: node,
+};
 
 export default InfoCards;
